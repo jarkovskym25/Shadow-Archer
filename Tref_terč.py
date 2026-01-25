728fca99fa808544e761753205218c9df82bfb0e
Matěj Jarkovský <jarkovskym25@spstrutnov.cz>
2026-01-25 18:01:47 +0100

    Místo střely, šíp!
    Vyměnil jsem za klasický obdélník svůj vlastně nakreslený šíp abych mohl dál přidat hráči luk z kterého bude šíp střílet.

--- Tref_terč.py
+++ Tref_terč.py
@@ -2,7 +2,6 @@ import sys
 import pygame
 import random
 pygame.init()
-konec_hry = False
 # Plocha
 rozliseni_okna = (800, 600)
 barva_pozadi = (255, 255, 255)
@@ -25,20 +24,11 @@ terc_y = (rozliseni_okna[1] - vyska_terce) // 2
 sirka_terce_2 = vyska_terce_2 = 50
 # Terč třetí
 sirka_terce_3 = vyska_terce_3 = 25
-# Přidání dalších čtverců na srdce
-uhel = 75
-uhel_2 = -75
-sirka_srdce = 15
-vyska_srdce = 25
-# Skore
-skore = 0
+# Počet peněz
+penize = 0
 #Informace pro střelu
-sirka_strely = 20
-vyska_strely = 5
-strela_x = ctverecek_x
-strela_y = ctverecek_y
-strela_leti = False
-rychlost_strely = 15
+strela_leti = False 
+rychlost_strely = 10
 # Čas terče
 terc_spawn_cas = pygame.time.get_ticks()
 terc_ceka = 1500
@@ -51,6 +41,10 @@ def nahodna_pozice_terce():
     return x, y
 # Hra stále běží
 konec_hry = False
+# Šíp
+obrazek_sipu = pygame.image.load("./sip_strela.png").convert_alpha()
+sip = pygame.transform.scale_by(obrazek_sipu, 0.1)
+sip_rect = sip.get_rect()
 # Srdíčko
 obrazek_srdicka = pygame.image.load("./srdicko.png").convert_alpha()
 srdicko = pygame.transform.scale_by(obrazek_srdicka, 0.1)
@@ -71,8 +65,7 @@ while True:
         if not konec_hry and udalost.type == pygame.KEYDOWN:
             if udalost.key == pygame.K_RIGHT and not strela_leti:
                 strela_leti = True
-                strela_x = ctverecek_x + velikost_ctverecku
-                strela_y = ctverecek_y + velikost_ctverecku // 2
+                sip_rect.center = (ctverecek_x + velikost_ctverecku, ctverecek_y + velikost_ctverecku // 2)
                 
      # Pohyb s ctvereckem pomocí šipek
     stisknute_klavesy = pygame.key.get_pressed()
@@ -96,19 +89,16 @@ while True:
     ctverecek_y = max(0, min(ctverecek_y, rozliseni_okna[1] - velikost_ctverecku))
     
     # Posun strely
-    if not konec_hry and strela_leti:
-        strela_x += rychlost_strely
-        if strela_x > rozliseni_okna[0]:
+    if strela_leti:
+        sip_rect.x += rychlost_strely
+        if sip_rect.left > rozliseni_okna[0]:
             strela_leti = False
-            terc_x, terc_y = nahodna_pozice_terce()
             pocet_srdicek -= 1
             
             if pocet_srdicek <= 0:
                 pocet_srdicek = 0
                 konec_hry = True
 
-
-            
     # Čas terče
     aktualni_cas = pygame.time.get_ticks()
     uplynulo = aktualni_cas - terc_spawn_cas
@@ -121,17 +111,14 @@ while True:
             terc_x, terc_y = nahodna_pozice_terce()
     
     # Kolize
-    strela_rect = pygame.Rect(strela_x, strela_y, sirka_strely, vyska_strely)
     terc_rect = pygame.Rect(terc_x, terc_y, sirka_terce, sirka_terce)
-
-    if strela_leti and faktor > 0 and strela_rect.colliderect(terc_rect):
-        skore += 10
+    if strela_leti and faktor > 0 and sip_rect.colliderect(terc_rect):
+        penize += 10
         strela_leti = False
         terc_x, terc_y = nahodna_pozice_terce()
         
     # Vyplněné plochy barvou
     okno.fill(barva_pozadi)
-    
     # Mínus srdíčko
     if pocet_srdicek > 0:
         okno.blit(srdicko, srdicko_rect)
@@ -140,7 +127,7 @@ while True:
     
     # Vytvoření čtverečku 
     pygame.draw.rect(okno, (50, 50, 50), (ctverecek_x, ctverecek_y, velikost_ctverecku, velikost_ctverecku))
-    # Vytvoření kruhů
+    # Vykreslení kruhů
     if faktor > 0:
         v1 = int(sirka_terce * faktor)
         v2 = int(sirka_terce_2 * faktor)
@@ -152,11 +139,11 @@ while True:
     pygame.draw.ellipse(okno, (200, 0, 0), (terc_x + o1, terc_y + o1, v1, v1))
     pygame.draw.ellipse(okno, (255, 255, 255), (terc_x + o2, terc_y + o2, v2, v2))
     pygame.draw.ellipse(okno, (200, 0, 0), (terc_x + o3, terc_y + o3, v3, v3))
-    # Vytvoření střely
+    # Vykreslení šipu
     if strela_leti:
-        pygame.draw.rect(okno, (255, 223, 100), (strela_x, strela_y, sirka_strely, vyska_strely))
+        okno.blit(sip, sip_rect)
     # Skore
-    text = font.render(f"Skóre: {skore}", True, (0, 0, 0))
+    text = font.render(f"Peníze: {penize}", True, (0, 0, 0))
     text_rect = text.get_rect(midtop=(rozliseni_okna[0] // 2, 10))
     okno.blit(text, text_rect)
     # Konec hry
